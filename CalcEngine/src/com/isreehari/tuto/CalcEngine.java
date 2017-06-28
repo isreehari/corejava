76139985ea0a77d5fc96279ad279fbf3302f5079
Sreehari Inukollu <hari.inukollu@ttu.edu>
2017-06-28 12:10:47 -0500

    Finished the inheritance.

--- CalcEngine/src/com/isreehari/tuto/CalcEngine.java
+++ CalcEngine/src/com/isreehari/tuto/CalcEngine.java
@@ -11,28 +11,50 @@ package com.isreehari.tuto;
  */
 public class CalcEngine {
 
+        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        double val1 = 100.0d;
-        double val2 = 50.0d;
-        double result;
-        char opCode = 'a';
+        MathEquation[] equations = new MathEquation[4];
         
-        if(opCode == 'a')
-            result = val1 + val2;
-        else if(opCode == 's')
-            result = val1 - val2;
-        else if(opCode == 'd')
-            result = val1 / val2;
-        else if( opCode == 'm')
-            result = val1 * val2;
-        else
-            result = 0.0d;
+        equations[0] = new MathEquation('a', 10.0d, 11.0d);
+        equations[1] = new MathEquation('s', 10.0d, 11.0d);
+        equations[2] = new MathEquation('m', 10.0d, 11.0d);
+        equations[3] = new MathEquation('d', 10.0d, 11.0d);
         
-        System.out.println(result);
+        for(MathEquation equation: equations){
+            equation.execute();            
+            System.out.println("Reult: ");
+            System.out.println(equation.getResultValue());
+            
+        }
+        
+        
+        System.out.println("***************************************");
+        System.out.println("Using Inheritance");
+        System.out.println("***************************************");
+        
+        CalculateBase[] calculators = new CalculateBase[4];
+        
+        calculators[0] = new Addition(10.0d, 11.0d);
+        calculators[1] = new Subtract(10.0d, 11.0d);
+        calculators[2] = new Multiple(10.0d, 11.0d);
+        calculators[3] = new Devision(10.0d, 11.0d);
+        
+        for(CalculateBase calculator: calculators){
+            calculator.execute();
+            System.out.println("Resut: " + calculator.getClass());
+            System.out.println(calculator.getResultValue());
+        }
+        
+        
+        
+        
+        
+        
+                
             
         
     }
